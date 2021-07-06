8ea1988c4ea399c6ea7a44a59d3749adad81209d
Ryan McConnell <ryanmcconnell.rm@gmail.com>
2021-07-06 13:31:33 +0100

    Implemented working solution with passing tests

--- arrayUtilities.js
+++ arrayUtilities.js
@@ -1,9 +1,22 @@
 //The function groupArrayElements takes an array, and an integer N 
 //and splits the given array into N sub arrays as evenly as possible
 function groupArrayElements(arr, N) {
-    let givenArray = arr
+    const givenArray = arr
+    const numberOfElementsPerGroup = Math.ceil((givenArray.length/N))
 
-    return new Array(N);
+    let returnedArray = new Array(N).fill([])
+
+    for(var i = 0; i < returnedArray.length; i++) {
+        let intermediateArray = []
+        for(var j = 0; j < numberOfElementsPerGroup; j++) {
+            if (!( ((i*numberOfElementsPerGroup) + j) > (givenArray.length-1))) {
+                intermediateArray.push(givenArray[(i*numberOfElementsPerGroup) + j])
+            }  
+        }
+        returnedArray[i] = [...intermediateArray]
+    }
+
+    return returnedArray;
 }
 
 module.exports = { groupArrayElements }
--- arrayUtilities.test.js
+++ arrayUtilities.test.js
@@ -1,13 +1,15 @@
+const { expect } = require('@jest/globals');
 const { groupArrayElements } = require('./arrayUtilities')
 
 //This is the testing suite for the groupArrayElements function
 describe('groupArrayElements', () => {
     it('should return an array with N sub arrays', () => {
-        const givenArray = [1,2,3,4,5]
+        const givenArray = [1,2,3,4,5,6,7,8,9,10]
         const N = 3
         
         const groupArrayElementsResult = groupArrayElements(givenArray, N)
 
+        //Expect the let of the returned array is equal to N
         expect(groupArrayElementsResult.length).toBe(3)
     });
 
@@ -16,10 +18,10 @@ describe('groupArrayElements', () => {
         const N = 3
         const groupArrayElementsResult = groupArrayElements(givenArray, N)
 
-        //This expects that the outer array contains N internal arrays where N is the given integer (3)
+        //Expects that the outer array contains N internal arrays where N is the given integer (3)
         expect(groupArrayElementsResult.length).toBe(N)
 
-        //This expects that the total length of the N internal arrays is the same as the original array
+        //Expects that the total length of the N internal arrays is the same as the original array
         let internalArrayLengthTotal = 0
         groupArrayElementsResult.forEach(subArray => {
             internalArrayLengthTotal += subArray.length
@@ -33,10 +35,10 @@ describe('groupArrayElements', () => {
         const N = 2
         const groupArrayElementsResult = groupArrayElements(givenArray, N)
 
-        //This expects that the outer array contains N internal sub arrays
+        //Expects that the outer array contains N internal sub arrays
         expect(groupArrayElementsResult.length).toBe(N)
 
-        //This expects that total length of all sub arrays equals the length of the given array
+        //Expects that total length of all sub arrays equals the length of the given array
         let internalArrayLengthTotal = 0
         groupArrayElementsResult.forEach(subArray => {
             internalArrayLengthTotal += subArray.length
@@ -45,18 +47,53 @@ describe('groupArrayElements', () => {
     });
 
     it('should handle the given array having a length of 0', () => {
-        const givenArray = [0]
+        const givenArray = []
         const N = 3
         const groupArrayElementsResult = groupArrayElements(givenArray, N)
 
-        //This expects that the outer array contains N internal sub arrays
+        //Expects that the outer array contains N internal sub arrays
         expect(groupArrayElementsResult.length).toBe(N)
 
-        //This expects that total length of all sub arrays equals the length of the given array
+        //Expects that total length of all sub arrays equals the length of the given array
         let internalArrayLengthTotal = 0
         groupArrayElementsResult.forEach(subArray => {
             internalArrayLengthTotal += subArray.length
         })
         expect(internalArrayLengthTotal).toBe(givenArray.length)
     });
+
+    it('should account for repeating numbers', () => {
+        const givenArray = [1,1,2,3,3,4,4,6,6,7,7,7]
+        const N = 4
+
+        const groupArrayElementsResult = groupArrayElements(givenArray, N)
+
+        //Expects that the outer array contains N internal sub arrays
+        expect(groupArrayElementsResult.length).toBe(N)
+
+        //Expects that the total length of all sub arrays equals the length of the given array
+        let internalArrayLengthTotal = 0
+        groupArrayElementsResult.forEach(subArray => {
+            internalArrayLengthTotal += subArray.length
+        })
+        expect(internalArrayLengthTotal).toBe(givenArray.length)
+    });
+
+    it('should account for numbers not being in order', () => {
+        const givenArray = [2,7,3,4,4,5,1,8,9]
+        const N = 4
+
+        const groupArrayElementsResult = groupArrayElements(givenArray, N)
+
+        //Expects that the outer array contains N internal sub arrays
+        expect(groupArrayElementsResult.length).toBe(N)
+        
+
+        //Expects thatthe total length of all sub arrays equals the length of the given array
+        let internalArrayLengthTotal = 0
+        groupArrayElementsResult.forEach(subArray => {
+            internalArrayLengthTotal += subArray.length
+        })
+        expect(internalArrayLengthTotal).toBe(givenArray.length)
+    })
 })
